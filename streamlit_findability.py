283341f10a789f5e137ac29ed6be7060656aae00
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-07 01:55:22 +0200

    Update streamlit_findability.py

--- streamlit_findability.py
+++ streamlit_findability.py
@@ -23,7 +23,7 @@ with dataset:
     originalSurvey = st.file_uploader('Upload survey database:', type=['sav'], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False)
     rtm_upload = st.file_uploader('Upload RTM database:', type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False)
     
-    if originalSurvey is None and rtm_upload is None:
+    if originalSurvey is None or rtm_upload is None:
         st.error("Please upload the files!")
     else:
         with open('temp.sav', "wb") as buffer:
