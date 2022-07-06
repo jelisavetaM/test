0888647e9e42737516890ca915c0ce3f569a11b4
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-07 01:40:30 +0200

    Update streamlit_findability.py

--- streamlit_findability.py
+++ streamlit_findability.py
@@ -11,6 +11,13 @@ import re
 import urllib.request, json
 from functools import reduce
 
-
-
-        
+dataset = st.container()
+	
+with dataset:
+    originalSurvey = st.file_uploader('Upload Databases from Virtual Shelf Platform:', type=['sav'], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False)
+    
+    if originalSurvey is None:
+        st.error("Please upload the files!")
+    else:
+        with open('temp.sav', "wb") as buffer:
+            shutil.copyfileobj(originalSurvey, buffer)
