e22e2b1091cb7f1706a97fd11017b7ed0e7959eb
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-07 02:15:43 +0200

    Update streamlit_findability.py

--- streamlit_findability.py
+++ streamlit_findability.py
@@ -425,10 +425,9 @@ with dataset:
         new_labels = new_labels + click_labels
         meta.variable_measure.update(click_measure)
         
-        with pd.ExcelWriter("final.xlsx") as writer:
-            finalAllMerged.to_excel(writer, sheet_name='sheet1', index=False)
-    
-        with open('final.xlsx', mode = "rb") as f:
-            st.download_button('Findability Data', f, file_name='final.xlsx')
-        
-        #pyreadstat.write_sav(finalAllMerged, os.getcwd() + '\\Outputs\\ETL\\Survey\\test_dapresy_final.sav',  variable_value_labels = new_dict, column_labels = new_labels, variable_measure = meta.variable_measure)
+	def get_table_download_link(df):
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+        get_table_download_link(get_table_download_link)
+        pyreadstat.write_sav(finalAllMerged, os.getcwd() + '\\Outputs\\ETL\\Survey\\test_dapresy_final.sav',  variable_value_labels = new_dict, column_labels = new_labels, variable_measure = meta.variable_measure)
