fae15352419185e6324a9749721ee6b6cdac40e2
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-07 02:27:07 +0200

    Update streamlit_findability.py

--- streamlit_findability.py
+++ streamlit_findability.py
@@ -425,12 +425,5 @@ with dataset:
                 
         new_labels = new_labels + click_labels
         meta.variable_measure.update(click_measure)
-        
-        def get_table_download_link(df):
-            csv = df.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-        
-
-        st.markdown(get_table_download_link(finalAllMerged), unsafe_allow_html=True)
-        #pyreadstat.write_sav(finalAllMerged, os.getcwd() + '\\Outputs\\ETL\\Survey\\test_dapresy_final.sav',  variable_value_labels = new_dict, column_labels = new_labels, variable_measure = meta.variable_measure)
+       
+        pyreadstat.write_sav(finalAllMerged, os.getcwd() + 'C:\\Users\\jelisaveta.m\\Desktop\\Decipher dashboards\\Outputs\\ETL\\Survey\\test_dapresy_final.sav',  variable_value_labels = new_dict, column_labels = new_labels, variable_measure = meta.variable_measure)
