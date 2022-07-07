dc7c083d4e5710ff3547b861906e65b0ba777576
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-07 03:01:00 +0200

    Update streamlit_findability.py

--- streamlit_findability.py
+++ streamlit_findability.py
@@ -426,7 +426,7 @@ with dataset:
         new_labels = new_labels + click_labels
         meta.variable_measure.update(click_measure)
         
-        with open("C:\\Users\\jelisaveta.m\\Desktop\\Decipher dashboards\\Outputs\\ETL\\Survey\\test_dapresy_final", "w") as f:
+        with open("C:\\Users\\jelisaveta.m\\Desktop\\Decipher dashboards\\Outputs\\ETL\\Survey\\test_dapresy_final.sav", "w") as f:
             #pyreadstat.write_sav(finalAllMerged, f)
             #pyreadstat.write_sav(finalAllMerged, f,  variable_value_labels = new_dict, column_labels = new_labels, variable_measure = meta.variable_measure)
             st.download_button('Findability Data', f, file_name='progress.sav')
